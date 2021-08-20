05eeece68368c55ccc073cc257714eda649dee2f
jdavid2019 <joseph2019d@gmail.com>
2021-08-19 20:53:15 -0500

    Vuex example course

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,23 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+    <h1>{{ $store.state.msg }}</h1>
+    <h2>{{ $store.state.data }}</h2>
+    <h3>Mi name is {{ $store.getters.getFullName }}</h3>
+    <h4>Mi lista de amigos</h4>
+    <input type="text" v-model="friend">
+    <button @click="addFriend">Agregar amigo</button>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li v-for="(friend, index) in $store.state.friends" :key="index">
+          {{ friend }}
+      </li>
     </ul>
-    <h3>Essential Links</h3>
+    <h4>Mi lista de cursos</h4>
+    <input type="text" v-model="add_course">
+    <button @click="addCourse">Agregar curso</button>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li v-for="(course, index) in $store.state.courses" :key="index">
+        {{ course }}
+      </li>
     </ul>
   </div>
 </template>
@@ -34,8 +25,23 @@
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      friend: '',
+      add_course: ''
+    }
+  },
+  methods: {
+    addFriend() {
+      this.$store.state.friend = this.friend
+      this.$store.dispatch('addFriendAction')
+      this.friend = ''
+    },
+    addCourse() {
+      this.$store.state.course = this.add_course
+      this.$store.dispatch('addCourseAction')
+      this.add_course = ''
+    }
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,45 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  // Global values of an application in vue
   state: {
+    'msg': 'Hello world',
+    'data': 'This is a new data',
+    'name': 'Joseph',
+    'lastname': 'Ninaquispe',
+    'friends': [],
+    'friend': null,
+    'courses': [],
+    'course': null
   },
+  // una mutacion se va realizar siempre y cuando sea llamado de una accion
+  // se va a utilizar para cambiar los datos del estado, pero para hacer llamar esta mutacion
+  // tengo que llamar a la action
   mutations: {
+   addFriend(state) {
+     state.friends = [state.friend,...state.friends]
+   },
+   addCourse(state) {
+     state.courses = [state.course,...state.courses]
+   }
   },
   actions: {
+    addFriendAction(context) {
+      context.commit('addFriend');
+    },
+    addCourseAction(context) {
+      context.commit('addCourse')
+    }
   },
-  modules: {
+  // Get state values
+  // Te permite los accesos a los estados
+  getters: {
+    getData(state) {
+      return state.data
+    },
+    getFullName(state) {
+      // comillas invertidas: alt gr + }
+      return `${state.name} ${state.lastname}`
+    }
   }
 })
